67defe97d8f741d9024f74ce800635584edb61f6
Abhay patel <you@example.com>
2026-02-20 17:47:08 +0530

    * eureka service registry

---
+++ eureka/src/test/java/com/fitness/edureka/EdurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fitness.edureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
